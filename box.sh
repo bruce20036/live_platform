7392bf2c95a7d395336bad4053304f652b4b31e9
hermitbruce <bruce20036@yahoo.com.tw>
2017-01-25 12:25:17 +0800

    Remove dash_web

---
+++ box.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+#  
+# /etc/init.d/box 
+#  
+# Starts the at daemon  
+#
+
+RAMDISK_SIZE=200
+RAMDISK_PATH=/tmp/hls 
+PACKAGE_PATH= /root/live_platform
+
+
+# Create ramdisk
+mkdir -p RAMDISK_PATH
+mount -t tmpfs -o size={$RAMDISK_SIZE}M tmpfs {$RAMDISK_PATH}
+
+
+
+
+
+
